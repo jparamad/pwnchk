c46eafb5b3d1216f94ab19f77b3f5de0cbf1a217
jparamad <70308152+jparamad@users.noreply.github.com>
2020-09-11 14:51:17 -0700

    Add files via upload
    v.0.2 - Added hashed file encoding

--- pwnchk.py
+++ pwnchk.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python
 # PwnChk - by John Paramadilok (2020.09)
-# This program checks passwords against those identified in HIBP to
-# be compromised.
+# This script checks sha1 hashed passwords against those identified
+# in HIBP be compromised.
 #
 # Dependencies:
-#   - os.subprocess uses shred
+#   - HIBP API, haveibeenpwned.com
+#   - os.system uses shred
 #   - requests package
 #
 # Notes:
 #   v.0.1 - Initial code development
+#   v.0.2 - Added hashed file encoding
+#
+# Menu Options:
+#    add      Adds passwd to list
+#    del      Deletes list
+#    h        Displays help menu
+#    q        Quit program
+#    num      Displays number of entries in list
+#    run      Runs program
+#
+# Output:
+#   run - evaluates hashed file list against HIBP API
+#       >> Searching db for (1) entries.
+#       << Prefix matches (21256)
+#       << Suffix matches (1)
+#       << ['2DC183F740EE76F27B78EB39C8AD972A757:54230']
+#   num - provides number of entries within the hashed file list
+#       << Returned (1) entries.
 #####################################################################
 
 import getpass
@@ -16,11 +35,10 @@ import hashlib
 import os
 import requests
 import base64
-import subprocess
 
 # Global Vars
-v = 'v.0.1'
-flist = './pwnchk.txt'  # Hashed list file
+v = 'v.0.2'
+flist = './pwnchk.enc'  # Encoded Hashed list file
 
 
 def fdecode_func():
@@ -28,20 +46,20 @@ def fdecode_func():
     with open(flist, 'r') as f:
         fcontent = f.read()
     fcontent_str = fcontent.encode('ascii')
-    fd = base64.b64decode(fcontent_str)
-    fdd = fd.decode('ascii')
-    return fdd
+    fde = base64.b64decode(fcontent_str)
+    fd = fde.decode('ascii')
+    return fd
 
 
-def fencode_func(log_data, fnew):
+def fencode_func(pass_hash):
     """Process Base Encoding of File Content"""
-    if (str(os.path.exists(flist)) == 'True') and (fnew == 0):
-        fdd = fdecode_func()
-        fd = fdd + log_data
+    if str(os.path.exists(flist)) == 'True':
+        fd = fdecode_func()
+        flist_hash = fd + '\n' + pass_hash
     else:
-        fd = log_data
-    fde = fd.encode('ascii')
-    fe = base64.b64encode(fde)
+        flist_hash = pass_hash
+    fhe = flist_hash.encode('ascii')
+    fe = base64.b64encode(fhe)
     fed = fe.decode('ascii')
     f = open(flist, 'w')
     f.write(fed)
@@ -51,9 +69,10 @@ def fencode_func(log_data, fnew):
 
 def hlist_func():
     """Process Base Decoding of Log File Content"""
-    with open(flist, 'r') as f:
-        fcontent = f.read()
-    hlist = fcontent.encode('ascii')
+    # with open(flist, 'r') as f:
+    #     fcontent = f.read()
+    hlist = fdecode_func()
+    hlist = hlist.encode('ascii')
     return hlist
 
 
@@ -66,20 +85,22 @@ def add_func():
     pass_str = passwd.encode('utf8')
     md.update(pass_str)
     pass_hash = md.hexdigest()
-    print('%pass_hash = ' + pass_hash)
+    # print('%pass_hash = ' + pass_hash)
 
-    """Clear passwd entry"""
+    """Clear passwd variable"""
     # print('%passwd = ' + passwd)
     passwd = ""
     # print('%passwd = ' + passwd)
 
-    f = open(flist, 'a')
-    f.write(pass_hash + '\n')
-    f.close()
+    fencode_func(pass_hash)
+
+    # f = open(flist, 'a')
+    # f.write(pass_hash + '\n')
+    # f.close()
 
 
 def del_func():
-    """Deletes list"""
+    """Deletes hash list"""
     if str(os.path.exists(flist)) != 'True':
         print('[ERROR: List does not exist]')
     else:
@@ -87,26 +108,14 @@ def del_func():
         if cin == 'y':
             # os.remove(flist)
             # print('<< ' + flist + ' removed.')
-            subprocess.run(['shred', '-fuvz', flist])
+            os.system('shred -fuvz ' + flist)
         else:
             print('[ABORT: List removal aborted.]')
 
 
-def num_func():
+
+def num_func(num):
     """Displays list count"""
-    if str(os.path.exists(flist)) != 'True':
-        print('[ERROR: List does not exist]')
-    else:
-        num = 0
-        with open(flist) as f:
-            for l in f:
-                l = l.strip('\n')
-                num += 1
-        print('<< Returned (' + str(num) + ') entries.')
-
-
-def run_func():
-    """Executes list checks against HIBP API"""
     if str(os.path.exists(flist)) != 'True':
         print('[ERROR: List does not exist]')
     else:
@@ -114,18 +123,33 @@ def run_func():
         hlist = hlist.upper()
         list = hlist.decode('utf8').strip()
         pass_list = []
+
+        # Converts entries in list variable to list
         for i in list.split('\n'):
             pass_list.append(i)
+
         list_len = len(pass_list)
-        print('>> Searching db for (' + str(list_len) + ') entries.')
+
+        if num == 0:
+            print('>> Searching db for (' + str(list_len) + ') entries.')
+            return pass_list
+        else:
+            print('>> List has (' + str(list_len) + ') entries.')
+
+
+def run_func():
+    """Executes list checks against HIBP API"""
+    if str(os.path.exists(flist)) != 'True':
+        print('[ERROR: List does not exist]')
+    else:
+        num=0
+        pass_list = num_func(num)
         m = 0
-        # n = 0
         for j in pass_list:
-            prefix = j[:5]
-            suffix = j[-35:]
+            prefix = j[:5]      # Based on HIBP API k-Anonymity model
+            suffix = j[-35:]    # To perform checks against returned values
 
             resp = requests.get('https://api.pwnedpasswords.com/range/' + prefix).text
-            # resp = requests.get('https://www.w3schools.com/python/demopage.htm').text
 
             print('<< Prefix matches (' + str(len(resp)) + ')')
             resp_list = []
@@ -157,7 +181,8 @@ def menu_func():
         elif cin == 'del':
             del_func()
         elif cin == 'num':
-            num_func()
+            num = 1
+            num_func(num)
         elif cin == 'run':
             run_func()
         elif cin == 'h':
